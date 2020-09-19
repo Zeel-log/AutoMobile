8cbfc7784ebc11e7298ff7dec15b97e0c7050ab1
Zeel-log <zeelm2014@gmail.com>
2020-09-19 19:44:06 +0530

    Updates

---
+++ precache-manifest.d0f771928b4e1b25fa611a8502216a08.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2caba90b70d3ee51693f6285c073dcb",
+    "url": "/AutoMobile/index.html"
+  },
+  {
+    "revision": "2f69d130af119f37a425",
+    "url": "/AutoMobile/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "59a4c2774fa7c0b0df20",
+    "url": "/AutoMobile/static/css/main.34ae7eb5.chunk.css"
+  },
+  {
+    "revision": "2f69d130af119f37a425",
+    "url": "/AutoMobile/static/js/2.e76f94b9.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/AutoMobile/static/js/2.e76f94b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a4c2774fa7c0b0df20",
+    "url": "/AutoMobile/static/js/main.f526ba7e.chunk.js"
+  },
+  {
+    "revision": "80c46035e514bdf72e3b",
+    "url": "/AutoMobile/static/js/runtime-main.ccf53c9f.js"
+  },
+  {
+    "revision": "10f5341a455ef4ef9cd5f19f26d8ea47",
+    "url": "/AutoMobile/static/media/aboutimg.10f5341a.jpg"
+  },
+  {
+    "revision": "4713aa09780b0911d3546a23d5e0bcb9",
+    "url": "/AutoMobile/static/media/brake.4713aa09.jpg"
+  },
+  {
+    "revision": "ad312d144fe8491c1fda3f68fe68a4c7",
+    "url": "/AutoMobile/static/media/car1.ad312d14.png"
+  },
+  {
+    "revision": "315a200f69053c8aa45f172c7f8eefd3",
+    "url": "/AutoMobile/static/media/carac.315a200f.jpg"
+  },
+  {
+    "revision": "2b1942fbf46f6c105b5dbe09805da2a6",
+    "url": "/AutoMobile/static/media/carepair.2b1942fb.jpg"
+  },
+  {
+    "revision": "e801405203e9c9ea160087bf07e2ca37",
+    "url": "/AutoMobile/static/media/engine.e8014052.jpg"
+  },
+  {
+    "revision": "714462247a032e1208d91bb4a2854e9e",
+    "url": "/AutoMobile/static/media/gear.71446224.jpg"
+  },
+  {
+    "revision": "6faf868adef9d63296472fc651ef4d4e",
+    "url": "/AutoMobile/static/media/oil.6faf868a.jpg"
+  },
+  {
+    "revision": "15f6c93416589bf6f9f03fb8262c9194",
+    "url": "/AutoMobile/static/media/steering.15f6c934.jpg"
+  }
+]);
